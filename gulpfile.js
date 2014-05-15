3d1c63b0152c280420986bf140b48d748b427dda
Miles Rausch <miles@milesrausch.com>
2014-05-15 15:43:11 -0500

    Added Express to search the site.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@ var gulp = require('gulp');
 
 var sass = require('gulp-ruby-sass');
 
+function startExpress() {
+
+    var express = require('express');
+    var app = express();
+
+    app.use(express.static(__dirname));
+    app.listen(4000);
+}
+
 gulp.task('sass', function(){
     gulp
         .src('./_assets/sass/*.scss')
@@ -9,4 +18,6 @@ gulp.task('sass', function(){
         .pipe( gulp.dest('./assets/styles') );
 });
 
-gulp.task('default', [ 'sass' ]);
+gulp.task('default', [ 'sass' ], function() {
+    startExpress();
+});
